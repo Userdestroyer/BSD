24e5347f1376555b843c85fc81ede350aedc1304
Userdestroyer <52831417+Userdestroyer@users.noreply.github.com>
2022-05-09 20:39:05 +0300

    Commit from Windows

---
+++ OpenCV_Windows/Savelev_ME_z-38/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    cv::VideoCapture* vid=nullptr;
+private slots:
+    void on_pushButton_clicked();
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
